9c7bd5e9e609384c0dbb8e54c204b19733823aed
LoicC <loloduvaucluse@gmail.com>
2024-02-28 16:59:44 +0100

    Ajout des imports des fichiers HTML ainsi que des images

--- init/front-end/src/app/scripts/score.js
+++ init/front-end/src/app/scripts/score.js
@@ -1,14 +1,14 @@
 import {parseUrl} from "./utils";
-// TODO #import-html: use ES default imports to import game.html as template
+import template from "../views/score.html";
   // TODO #class: use the ES6 class keyword
   /* class ScoreComponent constructor */
   export function ScoreComponent() {
     // TODO #extends: call super(template)
     var params = parseUrl();
-    // TODO #import-html: assign template to this.template
     this.name = params.name;
     this.size = parseInt(params.size);
     this.time = parseInt(params.time);
+    this.template = template;
   }
 
   // TODO #class: turn function into a method of ScoreComponent

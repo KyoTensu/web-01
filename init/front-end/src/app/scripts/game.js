a8f54358d4ff9022eb2c46a9451318210f077b19
LoicC <loloduvaucluse@gmail.com>
2024-03-05 11:13:20 +0100

    Mise en place des 'arrow function' et des 'template literals'

--- init/front-end/src/app/scripts/game.js
+++ init/front-end/src/app/scripts/game.js
@@ -45,8 +45,7 @@ let CARD_TEMPLATE = ""
     init(){
       // fetch the cards configuration from the server
       this.fetchConfig(
-          // TODO #arrow-function: use arrow function instead.
-          function (config) {
+          (config) => {
             this._config = config;
             this._boardElement = document.querySelector(".cards");
 
@@ -65,32 +64,28 @@ let CARD_TEMPLATE = ""
 
               card.getElement().addEventListener(
                   "click",
-                  // TODO #arrow-function: use arrow function instead.
-                  function () {
+                  () => {
                     this._flipCard(card);
-                  }.bind(this)
+                  }
               );
             }
 
             this.start();
-          }.bind(this)
+          }
       );
     }
 
     start(){
       this._startTime = Date.now();
       let seconds = 0;
-      // TODO #template-literals:  use template literals (backquotes)
       document.querySelector("nav .navbar-title").textContent =
-          "Player: " + this._name + ". Elapsed time: " + seconds++;
+          `Player: ${this._name}. Elapsed time: ${seconds}`;
 
       this._timer = setInterval(
-          // TODO #arrow-function: use arrow function instead.
-          function () {
-            // TODO #template-literals:  use template literals (backquotes)
+          () => {
             document.querySelector("nav .navbar-title").textContent =
-                "Player: " + this._name + ". Elapsed time: " + seconds++;
-          }.bind(this),
+                `Player: ${this._name}. Elapsed time: ${seconds}`;
+          },
           1000
       );
     }
@@ -101,11 +96,9 @@ let CARD_TEMPLATE = ""
               ? new XMLHttpRequest()
               : new ActiveXObject("Microsoft.XMLHTTP");
 
-      // TODO #template-literals:  use template literals (backquotes)
-      xhr.open("get", environment.api.host + "/board?size=" + this._size, true);
+      xhr.open("get", `${environment.api.host}/board?size=${this._size}`, true);
 
-      // TODO #arrow-function: use arrow function instead.
-      xhr.onreadystatechange = function () {
+      xhr.onreadystatechange = () => {
         let status;
         let data;
         // https://xhr.spec.whatwg.org/#dom-xmlhttprequest-readystate
@@ -130,19 +123,10 @@ let CARD_TEMPLATE = ""
       clearInterval(this._timer);
 
       setTimeout(
-          // TODO #arrow-function: use arrow function instead.
-          function () {
+          () => {
             let scorePage = "./#score";
-            // TODO #template-literals:  use template literals (backquotes)
-            window.location =
-                scorePage +
-                "?name=" +
-                this._name +
-                "&size=" +
-                this._size +
-                "&time=" +
-                timeElapsedInSeconds;
-          }.bind(this),
+            window.location = `${scorePage}?name=${this._name}&size=${this._size}&time=${timeElapsedInSeconds}`;
+          },
           750
       );
     }
@@ -184,8 +168,7 @@ let CARD_TEMPLATE = ""
           // cards did not match
           // wait a short amount of time before hiding both cards
           setTimeout(
-              // TODO #arrow-function: use arrow function instead.
-              function () {
+              () => {
                 // hide the cards
                 this._flippedCard.flip();
                 card.flip();
@@ -193,7 +176,7 @@ let CARD_TEMPLATE = ""
 
                 // reset flipped card for the next turn.
                 this._flippedCard = null;
-              }.bind(this),
+              },
               500
           );
         }
--- init/front-end/src/app/scripts/welcome.js
+++ init/front-end/src/app/scripts/welcome.js
@@ -12,8 +12,7 @@ import {Component} from "./component";
 
       form.addEventListener(
           "submit",
-          // TODO #arrow-function: use arrow function instead.
-          function (event) {
+          (event) => {
             event.preventDefault();
             if (form.checkValidity() === false) {
               event.stopPropagation();
@@ -24,7 +23,7 @@ import {Component} from "./component";
 
               this._startGame(name, size);
             }
-          }.bind(this),
+          },
           false
       );
 
@@ -33,7 +32,6 @@ import {Component} from "./component";
 
     _startGame(name, size){
       let gamePage = "./#game";
-      // TODO #template-literals:  use template literals (backquotes)
-      window.location = gamePage + "?name=" + name + "&size=" + size;
+      window.location = `${gamePage}?name=${name}&size=${size}`;
     }
   }

b660f8f12851cad4f080dc8ab0d5bccee3596318
LoicC <loloduvaucluse@gmail.com>
2024-03-06 15:36:07 +0100

    Mise en place de la Component Oriented Architecture

--- init/front-end/src/main.js
+++ init/front-end/src/main.js
@@ -1,7 +1,11 @@
 import { Router } from "./router";
-import { WelcomeComponent } from "./app/scripts/welcome";
-import { GameComponent } from "./app/scripts/game";
-import { ScoreComponent } from "./app/scripts/score";
+import { WelcomeComponent } from "./app/components/welcome/welcome.component";
+import { GameComponent } from "./app/components/game/game.component";
+import { ScoreComponent } from "./app/components/score/score.component";
+import {NavbarComponent} from "./app/components/navbar/navbar.component";
+customElements.define("my-navbar", NavbarComponent);
+import {FooterComponent} from "./app/components/footer/footer.component";
+customElements.define("my-footer", FooterComponent);
 
 import "/node_modules/bootstrap/dist/css/bootstrap.css";
 import "./app/styles/style.css";
